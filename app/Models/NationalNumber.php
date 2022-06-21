f03a5878a044550722c4f61013d7735005830c7e
Haidar Abdullah <haidar66pen11@gmail.com>
2022-06-21 11:11:48 +0300

    create models

---
+++ app/Models/NationalNumber.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NationalNumber extends Model
+{
+    use HasFactory;
+}
