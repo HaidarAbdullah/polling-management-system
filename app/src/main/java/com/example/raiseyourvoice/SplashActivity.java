1d9fffecc265f0f337ee92d9374c5723e413709c
Haidar Abdullah <haidar66pen11@gmail.com>
2022-07-16 09:58:05 +0300

    set intro image timer

--- app/src/main/java/com/example/raiseyourvoice/SplashActivity.java
+++ app/src/main/java/com/example/raiseyourvoice/SplashActivity.java
@@ -2,13 +2,32 @@ package com.example.raiseyourvoice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+    Thread timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        timer = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    synchronized (this) {
+                        wait(5000);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
