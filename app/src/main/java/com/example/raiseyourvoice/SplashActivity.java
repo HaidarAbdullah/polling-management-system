0bc1de968208fb58ae5fb5867d618c73eb3e7ad1
Haidar Abdullah <haidar66pen11@gmail.com>
2022-07-16 10:51:28 +0300

    add design effects

--- app/src/main/java/com/example/raiseyourvoice/SplashActivity.java
+++ app/src/main/java/com/example/raiseyourvoice/SplashActivity.java
@@ -2,17 +2,24 @@ package com.example.raiseyourvoice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class SplashActivity extends AppCompatActivity {
     Thread timer;
+    GifImageView splash_intro;
+    TextView tv;
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        timer = new Thread(){
+        timer = new Thread(){              //set intro image timer
             @Override
             public void run() {
                 try {
@@ -29,5 +36,9 @@ public class SplashActivity extends AppCompatActivity {
             }
         };
         timer.start();
+
+        tv = findViewById(R.id.tv);
+        tv.setVisibility(1);
+        tv.animate().translationY(100).setDuration(3000).setStartDelay(2000);
     }
 }
