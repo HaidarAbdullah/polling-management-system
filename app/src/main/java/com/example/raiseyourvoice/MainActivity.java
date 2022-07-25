b152b99c5168687f8d6cd4b47e89ce9470316fcd
Haidar Abdullah <haidar66pen11@gmail.com>
2022-07-25 11:37:12 +0300

    link elements with code

--- app/src/main/java/com/example/raiseyourvoice/MainActivity.java
+++ app/src/main/java/com/example/raiseyourvoice/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.raiseyourvoice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextInputLayout ilemail;
+    TextInputLayout ilpassword;
+    Button btn1;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       ilemail=findViewById(R.id.email_input_layout);
+        ilpassword=findViewById(R.id.password_input_layout);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ilemail.getEditText().getText().toString();
+                String password = ilpassword.getEditText().getText().toString();
+                Toast.makeText(MainActivity.this,email+" "+password,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
