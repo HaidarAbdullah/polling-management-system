8102fb74e3793737d4c2c931228214584cb57d4c
Haidar Abdullah <haidar66pen11@gmail.com>
2022-07-25 10:21:40 +0300

    add user functions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,5 +8,38 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
-    
+    public function register(Request $request) {
+
+        $user = User::create($request->all());
+        $user->password = Hash::make($user->password);
+        $user->save();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+            return response()->json([
+                'message' => 'credientials not match !'
+            ], 401);
+        }
+        $user = auth()->user();
+        return response()->json([
+            'token' => $user->createToken('token')->plainTextToken
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logged out successfully !'
+        ]);
+    }
 }
