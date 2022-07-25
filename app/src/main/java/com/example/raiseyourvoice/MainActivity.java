5f64c146b5dda579d6eb0c99b4fba5c6bc0580e5
Haidar Abdullah <haidar66pen11@gmail.com>
2022-07-25 13:01:39 +0300

    create Retrofit builder

--- app/src/main/java/com/example/raiseyourvoice/MainActivity.java
+++ app/src/main/java/com/example/raiseyourvoice/MainActivity.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     TextInputLayout ilemail;
@@ -20,7 +23,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       ilemail=findViewById(R.id.email_input_layout);
+        //use retrofit
+        Retrofit retrofit =new Retrofit.Builder()
+                .baseUrl("http://localhost:8000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiInterface apiInterface = retrofit.create(ApiInterface.class);
+        //use retrofit
+        ilemail=findViewById(R.id.email_input_layout);
         ilpassword=findViewById(R.id.password_input_layout);
         btn1=findViewById(R.id.btn1);
         btn2=findViewById(R.id.btn2);
