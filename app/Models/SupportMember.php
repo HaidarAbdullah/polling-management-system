46b5c44f00b49500f9e55ea3278eb223319d827e
Haidar Abdullah <haidar66pen11@gmail.com>
2022-08-02 11:07:48 +0300

    initialization

--- app/Models/SupportMember.php
+++ app/Models/SupportMember.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SupportMember extends Model
 {
     use HasFactory;
+    protected $table = "supportmembers";      // Rename table name if you want , it depends on your local DataBase
+    protected $fillable=['name','email','password'];
+    public $timestamps=false;
 }
